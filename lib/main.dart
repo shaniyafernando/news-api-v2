44cc6e237c7b6a56e512f6b0c290b06d27000e9b
Shaniya Fernando <fernandoshaniya99@gmail.com>
2023-10-18 23:19:57 +0530

    typography and color setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:new_api_v2/typography_theme_g.dart';
+import 'color_schemes.g.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -18,18 +20,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme,
+        textTheme: textTheme
       ),
+      darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme
+      ,textTheme: textTheme),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -108,6 +103,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            SelectableText("data",style: Theme.of(context).textTheme.displaySmall ,)
           ],
         ),
       ),
---
+++ lib/typography_theme_g.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const oswaldFontFamily =  TextStyle(
+  fontFamily: "Oswald"
+);
+
+const robotoFontFamily =  TextStyle(
+    fontFamily: "Roboto"
+);
+
+const textTheme = TextTheme(
+    displayLarge: robotoFontFamily,
+    displayMedium: robotoFontFamily,
+    displaySmall: robotoFontFamily,
+    headlineLarge: oswaldFontFamily,
+    headlineMedium: oswaldFontFamily,
+    headlineSmall: oswaldFontFamily,
+    titleLarge: oswaldFontFamily,
+    titleMedium: oswaldFontFamily,
+    titleSmall: oswaldFontFamily,
+    bodyLarge: robotoFontFamily,
+    bodyMedium: robotoFontFamily,
+    bodySmall:robotoFontFamily,
+    labelLarge: robotoFontFamily,
+    labelMedium: robotoFontFamily,
+    labelSmall:robotoFontFamily
+);
